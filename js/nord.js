cdd5f673b8004d6da4f028c8ed04b81e95ca2207
raz <elliot@raziel.space>
2019-06-14 22:39:56 +0100

    Finished arena except item rolls

--- js/nord.js
+++ js/nord.js
@@ -1,22 +1,30 @@
 nord = {
+  tools: ['arena','cart'],
+
   // initialise variables and other session details
   init: function() {
     console.log('Nordanner tools loaded');
     this.data = {};
-    this.data.ui = {};
+    // this.loadToolJS();
+    // rzl.loadJS("tools/arena/arena.js",true)
     this.drawUI({ui: 'tools'});
+  },
+
+  // load all of the JS files for nordanner tools
+  loadToolJS: function() {
+    return nord.tools.forEach(t => rzl.loadJS(`tools/${t}/${t}.js`));
   },
 
   // draw the UI - ui must be in this.nordui or args.ui
   drawUI: function(args) {
-    // if (!args || !args.nordui || !this.nordui) return;
+    if (!args && !args.nordui && !this.nordui) return;
     var ui;
     if (this.nordui) ui = this.nordui;
     else if (args && args.ui) ui = args.ui;
     else return;
     var def = nord.uiDefs[ui];
     if (typeof def === 'undefined') return;
-    nord.data.ui = new rzl.UI(def);
+    nord.data[ui] = new rzl.UI(def);
   },
 
   // generate a random number from 1 to n (inclusive)
@@ -32,44 +40,50 @@ nord = {
         name: 'tools',
       },
       view: {
-        0: {
-          type: 'block',
-          style: {},
-          children: {
-            0: {
-              tag: 'h1',
-              class: '',
-              style: {},
-              content: 'Choose a tool'
-            },
-            1: {
-              tag: 'button',
-              id: 'btnArenaTool',
-              content: 'Arena',
-              events: {
-                click: 'nord.drawUI'
+        // type: 'block',
+        style: {'max-width':'1080px',margin:'auto'},
+        children: [
+          // {
+          //   tag: 'h1',
+          //   class: '',
+          //   style: {margin:'auto'},
+          //   content: 'Choose a tool'
+          // },
+          {
+            tag: 'div',
+            id: 'nordToolbar',
+            style: {width:'100%'},
+            children: [
+              {
+                tag: 'button',
+                id: 'btnArenaTool',
+                content: 'Arena',
+                events: {
+                  click: 'nord.drawUI'
+                },
+                props: {
+                  nordui: 'arena'
+                }
               },
-              props: {
-                nordui: 'arena'
-              }
-            },
-            2: {
-              tag: 'button',
-              id: 'btnCartTool',
-              content: 'Cartography',
-              events: {
-                click: 'nord.drawUI'
+              {
+                tag: 'button',
+                id: 'btnCartTool',
+                content: 'Cartography',
+                events: {
+                  click: 'nord.drawUI'
+                },
+                props: {
+                  nordui: 'cartography'
+                }
               },
-              props: {
-                nordui: 'cartography'
-              }
-            },
-            3: {
-              tag: 'div',
-              id: 'norduiBox'
-            },
-          }
-        }
+            ]
+          },
+          {
+            tag: 'div',
+            id: 'norduiBox',
+            style: {width:'100%'}
+          },
+        ]
       }
     },
   }
