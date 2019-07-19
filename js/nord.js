9b30925295b9555ce0337d80fe985b93392eb7ac
raz <elliot@raziel.space>
2019-07-19 12:26:19 +0100

    load tool from url /#/<tool>

--- js/nord.js
+++ js/nord.js
@@ -5,9 +5,17 @@ nord = {
   init: function() {
     console.log("Nordanner tools loaded");
     this.data = {};
+
+    // retrieve # value from url
+    const hash = document.location.hash;
+    // get tool name
+    const actTool = this.data.activeTool = hash.split("/")[1];
+
     // this.loadToolJS();
     // rzl.loadJS("tools/arena/arena.js",true)
     this.drawUI({ui: "tools"});
+    // load tool if defined
+    if (actTool && nord.uiDefs[actTool]) this.drawUI({ui: actTool});
   },
 
   // load all of the JS files for nordanner tools
@@ -22,6 +30,8 @@ nord = {
     if (this.nordui) ui = this.nordui;
     else if (args && args.ui) ui = args.ui;
     else return;
+    const hash = (ui === "tools") ? "" : ui;
+    document.location.hash = "#/" + hash;
     const def = nord.uiDefs[ui];
     if (typeof def === "undefined") return;
     new rzl.UI(def);
